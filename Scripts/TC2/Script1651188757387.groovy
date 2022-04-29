17a10c7a4adee721b834c6cfd7871c570d14cea0
kazurayam <kazuaki.urayama@gmail.com>
2022-04-29 09:48:19 +0900

    prettify TC1 and TC2

--- Scripts/TC2/Script1651188757387.groovy
+++ Scripts/TC2/Script1651188757387.groovy
@@ -1,24 +1,9 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.testobject.ConditionType
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-import org.openqa.selenium.WebElement
 import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
+
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 File html = new File("./Households - Index.mhtml")
 
@@ -31,13 +16,11 @@ assert rows.size() > 1
 int numberOfRows = rows.size()
 println "numberOfRows=${numberOfRows}"
 
-//
 for (WebElement row in rows) {
 	WebElement owner = row.findElement(By.xpath("div[@role='gridcell'][4]"))
 	assert owner != null
-	println "owner=${owner.getText()}"
-	WebElement status = row.findElement(By.xpath("div[@role='gridcell'][5]"))
-	
+	//println "owner=${owner.getText()}"
+	WebElement status = row.findElement(By.xpath("div[@role='gridcell'][5]"))	
 	if (owner.getText().trim() == "Ashraf Tester" && 
 		status.getText().trim() == "Client") {
 		checkRow(row)
@@ -52,7 +35,7 @@ WebUI.closeBrowser()
 def checkRow(WebElement row) {
 	WebElement name = row.findElement(By.xpath("div[@role='gridcell'][3]"))
 	WebUI.comment("Name=${name.getText().trim()}")
-	// and more ...
+	// add more code ...
 }
 
 /**
